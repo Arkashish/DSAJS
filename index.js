af089f69b5440fa18cae9ed599e42ab7c3dd1e78
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-04-16 18:47:42 +0530

    Added codes for Bubblesort,Mergesort and Quicksort under Algo Folder  and Heap,Queue under DS Folder

---
+++ index.js
@@ -0,0 +1,20 @@
+// const {PriorityQueue} = require('./dataStructures/index')
+
+// const pq = new PriorityQueue((a,b)=>a-b);
+
+// pq.insert(10);
+// pq.insert(20);
+// pq.insert(30);
+// pq.insert(40);
+
+// console.log(pq.get());
+
+// pq.display();
+
+const DataStructures = require('./dataStructures/index');
+const Algorithms = require('./algorithms/index');
+
+module.exports = {
+    DataStructures,
+    Algorithms
+}
